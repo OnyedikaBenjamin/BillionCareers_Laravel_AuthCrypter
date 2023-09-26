351c3cf1e7c6da1c260a002c6a5b30de7d92b6bd
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-26 13:47:57 +0100

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,3 +28,29 @@ class UserController extends Controller
         return redirect('/')->with('message', 'Account created successfully');
     }
 }
+
+@auth
+      <li>
+        <span class="font-bold uppercase">
+          Welcome {{auth()->user()->name}}
+        </span>
+      </li>
+      <li>
+        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+      </li>
+      <li>
+        <form class="inline" method="POST" action="/logout">
+          @csrf
+          <button type="submit">
+            <i class="fa-solid fa-door-closed"></i> Logout
+          </button>
+        </form>
+      </li>
+      @else
+      <li>
+        <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+      </li>
+      <li>
+        <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+      </li>
+      @endauth
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,30 +1,31 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-         <title>Job board HTML-5 Template </title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="manifest" href="site.webmanifest">
-		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
-		<!-- CSS here -->
-            <link rel="stylesheet" href="{{ asset('assets/css/slicknav.css')}}">
-            <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
-            <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/price_rangs.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css')}}">
-            <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
-            <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-   </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Job board HTML-5 Template </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
-   <body>
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('assets/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/price_rangs.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+
+<body>
     <!-- Preloader Start -->
     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
@@ -39,15 +40,15 @@
     <!-- Preloader End -->
     <header>
         <!-- Header Start -->
-       <div class="header-area header-transparrent">
-           <div class="headder-top header-sticky">
+        <div class="header-area header-transparrent">
+            <div class="headder-top header-sticky">
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-lg-3 col-md-2">
                             <!-- Logo -->
                             <div class="logo">
                                 <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
-                            </div>  
+                            </div>
                         </div>
                         <div class="col-lg-9 col-md-9">
                             <div class="menu-wrapper">
@@ -69,12 +70,18 @@
                                             <li><a href="contact.html">Contact</a></li>
                                         </ul>
                                     </nav>
-                                </div>          
+                                </div>
                                 <!-- Header-btn -->
+                                @auth
+                                <div class="header-btn d-none f-right d-lg-block">
+                                    <h2>Welcome {{auth()->user()->name}}</h2>
+                                </div>
+                                @else
                                 <div class="header-btn d-none f-right d-lg-block">
                                     <a href="/register" class="btn head-btn1">Register</a>
                                     <a href="/login" class="btn head-btn2">Login</a>
                                 </div>
+                                @endauth
                             </div>
                         </div>
                         <!-- Mobile Menu -->
@@ -83,15 +90,16 @@
                         </div>
                     </div>
                 </div>
-           </div>
-       </div>
+            </div>
+        </div>
         <!-- Header End -->
     </header>
     <main>
 
         <!-- Hero Area Start-->
         <div class="slider-area ">
-            <div class="single-slider section-overly slider-height2 d-flex align-items-center" data-background="assets/img/hero/about.jpg">
+            <div class="single-slider section-overly slider-height2 d-flex align-items-center"
+                data-background="assets/img/hero/about.jpg">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
@@ -112,14 +120,12 @@
                     <div class="col-xl-3 col-lg-3 col-md-4">
                         <div class="row">
                             <div class="col-12">
-                                    <div class="small-section-tittle2 mb-45">
-                                    <div class="ion"> <svg 
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                                        width="20px" height="12px">
-                                    <path fill-rule="evenodd"  fill="rgb(27, 207, 107)"
-                                        d="M7.778,12.000 L12.222,12.000 L12.222,10.000 L7.778,10.000 L7.778,12.000 ZM-0.000,-0.000 L-0.000,2.000 L20.000,2.000 L20.000,-0.000 L-0.000,-0.000 ZM3.333,7.000 L16.667,7.000 L16.667,5.000 L3.333,5.000 L3.333,7.000 Z"/>
-                                    </svg>
+                                <div class="small-section-tittle2 mb-45">
+                                    <div class="ion"> <svg xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="12px">
+                                            <path fill-rule="evenodd" fill="rgb(27, 207, 107)"
+                                                d="M7.778,12.000 L12.222,12.000 L12.222,10.000 L7.778,10.000 L7.778,12.000 ZM-0.000,-0.000 L-0.000,2.000 L20.000,2.000 L20.000,-0.000 L-0.000,-0.000 ZM3.333,7.000 L16.667,7.000 L16.667,5.000 L3.333,5.000 L3.333,7.000 Z" />
+                                        </svg>
                                     </div>
                                     <h4>Filter Jobs</h4>
                                 </div>
@@ -129,9 +135,9 @@
                         <div class="job-category-listing mb-50">
                             <!-- single one -->
                             <div class="single-listing">
-                               <div class="small-section-tittle2">
-                                     <h4>Job Category</h4>
-                               </div>
+                                <div class="small-section-tittle2">
+                                    <h4>Job Category</h4>
+                                </div>
                                 <!-- Select job items start -->
                                 <div class="select-job-items2">
                                     <select name="select">
@@ -149,7 +155,7 @@
                                         <h4>Job Type</h4>
                                     </div>
                                     <label class="container">Full Time
-                                        <input type="checkbox" >
+                                        <input type="checkbox">
                                         <span class="checkmark"></span>
                                     </label>
                                     <label class="container">Part Time
@@ -169,9 +175,9 @@
                             </div>
                             <!-- single two -->
                             <div class="single-listing">
-                               <div class="small-section-tittle2">
-                                     <h4>Job Location</h4>
-                               </div>
+                                <div class="small-section-tittle2">
+                                    <h4>Job Location</h4>
+                                </div>
                                 <!-- Select job items start -->
                                 <div class="select-job-items2">
                                     <select name="select">
@@ -189,7 +195,7 @@
                                         <h4>Experience</h4>
                                     </div>
                                     <label class="container">1-3 Years
-                                        <input type="checkbox" >
+                                        <input type="checkbox">
                                         <span class="checkmark"></span>
                                     </label>
                                     <label class="container">3-5 Years
@@ -211,7 +217,7 @@
                                         <h4>Posted Within</h4>
                                     </div>
                                     <label class="container">Any
-                                        <input type="checkbox" >
+                                        <input type="checkbox">
                                         <span class="checkmark"></span>
                                     </label>
                                     <label class="container">Today
@@ -260,7 +266,7 @@
                                         </div>
                                     </div>
                                 </aside>
-                              <!-- Range Slider End -->
+                                <!-- Range Slider End -->
                             </div>
                         </div>
                         <!-- Job Category Listing End -->
@@ -333,7 +339,8 @@
                                     <li class="page-item active"><a class="page-link" href="#">01</a></li>
                                     <li class="page-item"><a class="page-link" href="#">02</a></li>
                                     <li class="page-item"><a class="page-link" href="#">03</a></li>
-                                <li class="page-item"><a class="page-link" href="#"><span class="ti-angle-right"></span></a></li>
+                                    <li class="page-item"><a class="page-link" href="#"><span
+                                                class="ti-angle-right"></span></a></li>
                                 </ul>
                             </nav>
                         </div>
@@ -342,45 +349,46 @@
             </div>
         </div>
         <!--Pagination End  -->
-        
+
     </main>
 
-	<!-- JS here -->
-	
-		<!-- All JS Custom Plugins Link Here here -->
-        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
-		<!-- Jquery, Popper, Bootstrap -->
-		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-        <script src="./assets/js/popper.min.js"></script>
-        <script src="./assets/js/bootstrap.min.js"></script>
-	    <!-- Jquery Mobile Menu -->
-        <script src="./assets/js/jquery.slicknav.min.js"></script>
+    <!-- JS here -->
 
-		<!-- Jquery Slick , Owl-Carousel Range -->
-        <script src="./assets/js/owl.carousel.min.js"></script>
-        <script src="./assets/js/slick.min.js"></script>
-        <script src="./assets/js/price_rangs.js"></script>
-		<!-- One Page, Animated-HeadLin -->
-        <script src="./assets/js/wow.min.js"></script>
-		<script src="./assets/js/animated.headline.js"></script>
-        <script src="./assets/js/jquery.magnific-popup.js"></script>
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
 
-		<!-- Scrollup, nice-select, sticky -->
-        <script src="./assets/js/jquery.scrollUp.min.js"></script>
-        <script src="./assets/js/jquery.nice-select.min.js"></script>
-		<script src="./assets/js/jquery.sticky.js"></script>
-        
-        <!-- contact js -->
-        <script src="./assets/js/contact.js"></script>
-        <script src="./assets/js/jquery.form.js"></script>
-        <script src="./assets/js/jquery.validate.min.js"></script>
-        <script src="./assets/js/mail-script.js"></script>
-        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
-        
-		<!-- Jquery Plugins, main Jquery -->	
-        <script src="./assets/js/plugins.js"></script>
-        <script src="./assets/js/main.js"></script>
-        
-    </body>
-    @extends('footer')
+    <!-- Jquery Slick , Owl-Carousel Range -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <script src="./assets/js/price_rangs.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="./assets/js/jquery.scrollUp.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+</body>
+@extends('footer')
+
 </html>
