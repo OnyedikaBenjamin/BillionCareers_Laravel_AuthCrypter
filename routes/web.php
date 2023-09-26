26d2629ca8abf2ef6408ee725c50e79b0e69a979
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-26 15:37:00 +0100

    updated

--- routes/web.php
+++ routes/web.php
@@ -7,33 +7,24 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Tests\Integration\Database\EloquentModelDateCastingTest\EloquentModelImmutableDateCastingTest;
 
-// Common Resource Routes:
-// index - Show all listings
-// show - Show single listing
-// create - Show form to create new listing
-// store - Store new listing
-// edit - Show form to edit listing
-// update - Update listing
-// destroy - Delete listing  
-
 //LISTINGS ROUTE
 Route::get('/', [ListingController::class, 'showAllListing']);
 
 Route::get('/listings/{id}', [ListingController::class, 'showSingleListing']);
 
-Route::get('/create', [ListingController::class, 'createListing']);
+Route::get('/create', [ListingController::class, 'createListing'])->middleware('auth');
 
-Route::post('/listings', [ListingController::class, 'storeListing']);
+Route::post('/listings', [ListingController::class, 'storeListing'])->middleware('auth');
 
 Route::get('/jobcreated', [ListingController::class, 'jobPostSuccessful']);
 
 Route::get('/jobupdated', [ListingController::class, 'jobPostUpdated']);
 
-Route::get('listings/{listing}/edit', [ListingController::class, 'editListing']);
+Route::get('listings/{listing}/edit', [ListingController::class, 'editListing'])->middleware('auth');
 
-Route::put('listings/{listing}', [ListingController::class, 'updateListing']);
+Route::put('listings/{listing}', [ListingController::class, 'updateListing'])->middleware('auth');
 
-Route::delete('listings/{listing}', [ListingController::class, 'deleteListing']);
+Route::delete('listings/{listing}', [ListingController::class, 'deleteListing'])->middleware('auth');
 
 // USER ROUTES
 
