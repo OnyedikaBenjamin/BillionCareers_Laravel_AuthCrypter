56a6f03c009774a7161c880c091dc2e6a2daa616
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-28 18:15:47 +0100

    updated

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -38,7 +38,7 @@
         </div>
     </div>
     <!-- Preloader End -->
-    <header>
+    {{-- <header>
         <!-- Header Start -->
         <div class="header-area header-transparrent">
             <div class="headder-top header-sticky">
@@ -73,13 +73,12 @@
                                 </div>
                                 <!-- Header-btn -->
                                 @auth
-                                {{-- <div class="header-btn d-none f-right d-lg-block"> --}}
+                                {{-- <div class="header-btn d-none f-right d-lg-block"> 
                                     <h4 style="color: rgb(0, 110, 255)">{{auth()->user()->name}}</h4>
                                     <form class="inline" method="POST" action="/logout">
                                         @csrf
                                         <button href="/login" class="btn head-btn2" type="submit">Logout</button>
                                       </form>
-                                {{-- </div> --}}
                                 @else
                                 <div class="header-btn d-none f-right d-lg-block">
                                     <a href="/users/register" class="btn head-btn1">Register</a>
@@ -97,7 +96,54 @@
             </div>
         </div>
         <!-- Header End -->
-    </header>
+    </header> --}}
+    <header class="site-navbar mt-3">
+        <div class="container-fluid">
+          <div class="row align-items-center">
+            <div class="site-logo col-6"><a href="index.html">JobBoard</a></div>
+  
+            <nav class="mx-auto site-navigation">
+              <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+                <li><a href="index.html" class="nav-link active">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li class="has-children">
+                  <a href="job-listings.html">Job Listings</a>
+                  <ul class="dropdown">
+                    <li><a href="job-single.html">Job Single</a></li>
+                    <li><a href="post-job.html">Post a Job</a></li>
+                  </ul>
+                </li>
+                <li class="has-children">
+                  <a href="services.html">Pages</a>
+                  <ul class="dropdown">
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="service-single.html">Service Single</a></li>
+                    <li><a href="blog-single.html">Blog Single</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="portfolio-single.html">Portfolio Single</a></li>
+                    <li><a href="testimonials.html">Testimonials</a></li>
+                    <li><a href="faq.html">Frequently Ask Questions</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                  </ul>
+                </li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
+                <li class="d-lg-none"><a href="login.html">Log In</a></li>
+              </ul>
+            </nav>
+            
+            <div class="right-cta-menu text-right d-flex aligin-items-center col-6">
+              <div class="ml-auto">
+                <a href="post-job.html" class="btn btn-outline-white border-width-2 d-none d-lg-inline-block"><span class="mr-2 icon-add"></span>Post a Job</a>
+                <a href="login.html" class="btn btn-primary border-width-2 d-none d-lg-inline-block"><span class="mr-2 icon-lock_outline"></span>Log In</a>
+              </div>
+              <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
+            </div>
+  
+          </div>
+        </div>
+      </header>
     <main>
 
         <!-- Hero Area Start-->
