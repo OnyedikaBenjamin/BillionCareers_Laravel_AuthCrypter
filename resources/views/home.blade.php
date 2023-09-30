bdd6c4138b4395b130182129c8ea91bf219e6556
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-30 23:45:14 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -90,6 +90,7 @@
                     <select class="selectpicker" data-style="btn-white btn-lg" data-width="100%" data-live-search="true" title="Select Region" tabindex="-98">
                     <option>Anywhere</option>
                     <option>San Francisco</option>
+                    <option>Palo Alto</option>
                     <option>New York</option>
                     <option>Manhattan</option>
                     <option>Ontario</option>
