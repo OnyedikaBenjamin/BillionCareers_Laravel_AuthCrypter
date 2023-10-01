b647dfbc38f9a5d93d25a338165f7da4ab4ce783
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-01 07:52:36 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -617,29 +617,8 @@
       </div>
 
     </section>
-
-    <section class="pt-5 bg-image overlay-primary fixed overlay" style="background-image: url('images/hero_1.jpg');">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 align-self-center text-center text-md-left mb-5 mb-md-0">
-            <h2 class="text-white">Get The Mobile Apps</h2>
-            <p class="mb-5 lead text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit tempora adipisci impedit.</p>
-            <p class="mb-0">
-              <a href="#" class="btn btn-dark btn-md px-4 border-width-2"><span class="icon-apple mr-3"></span>App Store</a>
-              <a href="#" class="btn btn-dark btn-md px-4 border-width-2"><span class="icon-android mr-3"></span>Play Store</a>
-            </p>
-          </div>
-          <div class="col-md-6 ml-auto align-self-end">
-            <img src="images/apps.png" alt="Free Website Template by Free-Template.co" class="img-fluid">
-          </div>
-        </div>
-      </div>
-    </section>
-    
-    @extends('footer')
-  
   </div>
-
+  @extends('footer')
     <script src="{{ asset('js/js-post/jquery.min.js') }}"></script>
 <script src="{{ asset('js/js-post/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('js/js-post/isotope.pkgd.min.js') }}"></script>
