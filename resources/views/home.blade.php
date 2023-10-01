f90831fba49d962a7ec40ce52a098dfa8704acce
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-01 07:49:55 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -578,47 +578,7 @@
 </main>
 
     
-    <section class="site-section py-4">
-      <div class="container">
-  
-        <div class="row align-items-center">
-          <div class="col-12 text-center mt-4 mb-5">
-            <div class="row justify-content-center">
-              <div class="col-md-7">
-                <h2 class="section-title mb-2">Company We've Helped</h2>
-                <p class="lead">Porro error reiciendis commodi beatae omnis similique voluptate rerum ipsam fugit mollitia ipsum facilis expedita tempora suscipit iste</p>
-              </div>
-            </div>
-            
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_mailchimp.svg" alt="Image" class="img-fluid logo-1">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_paypal.svg" alt="Image" class="img-fluid logo-2">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_stripe.svg" alt="Image" class="img-fluid logo-3">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_visa.svg" alt="Image" class="img-fluid logo-4">
-          </div>
-
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_apple.svg" alt="Image" class="img-fluid logo-5">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_tinder.svg" alt="Image" class="img-fluid logo-6">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_sony.svg" alt="Image" class="img-fluid logo-7">
-          </div>
-          <div class="col-6 col-lg-3 col-md-6 text-center">
-            <img src="images/logo_airbnb.svg" alt="Image" class="img-fluid logo-8">
-          </div>
-        </div>
-      </div>
-    </section>
+    
 
 
     <section class="bg-light pt-5 testimony-full">
