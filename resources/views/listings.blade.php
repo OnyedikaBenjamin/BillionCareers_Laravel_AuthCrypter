d704e31705010fd682f88fd76b9f0590f5e55457
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-01 07:37:55 +0100

    updated

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -37,7 +37,7 @@
 
 <body>
     <!-- Preloader Start -->
-    {{-- <div id="preloader-active">
+    <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
@@ -46,7 +46,7 @@
                 </div>
             </div>
         </div>
-    </div> --}}
+    </div>
     
     <!-- Preloader End -->
     @extends('header')
