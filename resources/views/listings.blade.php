18d6d77dbb351a3243237131a8861955e7a354cf
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-01 01:33:34 +0100

    updated

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -313,44 +313,42 @@
         <!--Pagination End  -->
 
     </main>
-
-    <!-- JS here -->
-
-    <!-- All JS Custom Plugins Link Here here -->
-    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+@extends('footer')
+    <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js')}}"></script>
     <!-- Jquery, Popper, Bootstrap -->
-    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{ asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
     <!-- Jquery Mobile Menu -->
-    <script src="./assets/js/jquery.slicknav.min.js"></script>
+    <script src="{{ asset('assets/js/jquery.slicknav.min.js')}}"></script>
 
-    <!-- Jquery Slick , Owl-Carousel Range -->
-    <script src="./assets/js/owl.carousel.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
-    <script src="./assets/js/price_rangs.js"></script>
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('assets/js/slick.min.js')}}"></script>
+    <script src="{{ asset('assets/js/price_rangs.js')}}"></script>
+    
     <!-- One Page, Animated-HeadLin -->
-    <script src="./assets/js/wow.min.js"></script>
-    <script src="./assets/js/animated.headline.js"></script>
-    <script src="./assets/js/jquery.magnific-popup.js"></script>
+    <script src="{{ asset('assets/js/wow.min.js')}}"></script>
+    <script src="{{ asset('assets/js/animated.headline.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.js')}}"></script>
 
     <!-- Scrollup, nice-select, sticky -->
-    <script src="./assets/js/jquery.scrollUp.min.js"></script>
-    <script src="./assets/js/jquery.nice-select.min.js"></script>
-    <script src="./assets/js/jquery.sticky.js"></script>
-
+    <script src="{{ asset('assets/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.sticky.js')}}"></script>
+    
     <!-- contact js -->
-    <script src="./assets/js/contact.js"></script>
-    <script src="./assets/js/jquery.form.js"></script>
-    <script src="./assets/js/jquery.validate.min.js"></script>
-    <script src="./assets/js/mail-script.js"></script>
-    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="{{ asset('assets/js/contact.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.form.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.validate.min.js')}}"></script>
+    <script src="{{ asset('assets/js/mail-script.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.ajaxchimp.min.js')}}"></script>
+    
+    <!-- Jquery Plugins, main Jquery -->	
+    <script src="{{ asset('assets/js/plugins.js')}}"></script>
+    <script src="{{ asset('assets/js/main.js')}}"></script>
 
-    <!-- Jquery Plugins, main Jquery -->
-    <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/main.js"></script>
+
 
 </body>
-@extends('footer')
-
 </html>
