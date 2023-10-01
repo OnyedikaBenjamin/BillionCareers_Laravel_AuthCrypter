dcbbf107f771db5091517579c9f65deda1fc8605
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-01 17:16:32 +0100

    updated

--- resources/views/singleListing.blade.php
+++ resources/views/singleListing.blade.php
@@ -22,74 +22,23 @@
             <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
             <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
             <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+            <link rel="stylesheet" href="{{ asset('assets/css-post/custom-bs.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/jquery.fancybox.min.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/bootstrap-select.min.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/owl.carousel.min.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/animate.min.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/quill.snow.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/css-post/style.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css')}}">
+            <link rel="stylesheet" href="{{ asset('assets/fonts/line-icons/style.css')}}">
+            <link rel="stylesheet" href="{{'assets/css-post/style.css'}}">
    
    </head>
-
+ @extends('header')
    <body>
-    <!-- Preloader Start -->
-    {{-- <div id="preloader-active">
-        <div class="preloader d-flex align-items-center justify-content-center">
-            <div class="preloader-inner position-relative">
-                <div class="preloader-circle"></div>
-                <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/logo.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    <!-- Preloader Start -->
-    <header>
-        <!-- Header Start -->
-       <div class="header-area header-transparrent">
-           <div class="headder-top header-sticky">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-3 col-md-2">
-                            <!-- Logo -->
-                            <div class="logo">
-                                <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
-                            </div>  
-                        </div>
-                        <div class="col-lg-9 col-md-9">
-                            <div class="menu-wrapper">
-                                <!-- Main-menu -->
-                                <div class="main-menu">
-                                    <nav class="d-none d-lg-block">
-                                        <ul id="navigation">
-                                            <li><a href="index.html">Home</a></li>
-                                            <li><a href="job_listing.html">Find a Jobs </a></li>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="#">Page</a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="single-blog.html">Blog Details</a></li>
-                                                    <li><a href="elements.html">Elements</a></li>
-                                                    <li><a href="job_details.html">job Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="contact.html">Contact</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>          
-                                <!-- Header-btn -->
-                                <div class="header-btn d-none f-right d-lg-block">
-                                    <a href="#" class="btn head-btn1">Register</a>
-                                    <a href="#" class="btn head-btn2">Login</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Mobile Menu -->
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
-                </div>
-           </div>
-       </div>
-        <!-- Header End -->
-    </header>
+  
     <main>
-
         <!-- Hero Area Start-->
         {{-- <div class="slider-area ">
         <div class="single-slider section-overly slider-height2 d-flex align-items-center" data-background="assets/img/hero/about.jpg">
