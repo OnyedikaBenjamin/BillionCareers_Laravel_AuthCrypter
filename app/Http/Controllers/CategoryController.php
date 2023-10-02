a028daa4fa1e7b65bd9233305b9faedab39962e8
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-02 13:19:32 +0100

    updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,3 +9,61 @@ class CategoryController extends Controller
 {
     //
 }
+
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:categories|max:255',
+            'description' => 'required',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Category created successfully');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|max:255|unique:categories,name,' . $category->id,
+            'description' => 'required',
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('success', 'Category deleted successfully');
+    }
+}
