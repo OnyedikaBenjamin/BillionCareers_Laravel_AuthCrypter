4e2ff65a200a9f3689e07870d3849853b20960e8
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-03 07:42:52 +0100

    updated

--- resources/views/singleListing.blade.php
+++ resources/views/singleListing.blade.php
@@ -193,7 +193,7 @@
                                 <a href="job_details.html"><img src="assets/img/icon/job-list1.png" alt=""></a>
                             </div>
                             <div class="job-tittle">
-                                <a href="job_details.html"><h4></h4></a>
+                                <a href="job_details.html"><h4>Digital Marketer</h4></a>
                                 <ul>
                                     <li>Creative Agency</li>
                                     <li><i class="fas fa-map-marker-alt"></i>Athens, Greece</li>
