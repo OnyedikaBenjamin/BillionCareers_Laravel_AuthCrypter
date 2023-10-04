6ad566397f8df3776a4f7ea3770ff40b9d6548bf
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-04 08:04:53 +0100

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -53,7 +53,7 @@ class UserController extends Controller
 
     public function savedJobs()
     {
-        return $this->belongsToMany(Job::class, 'saved_job_user', 'user_id', 'job_id')->withTimestamps();
+        return $this->belongsToMany(Listing::class, 'saved_job_user', 'user_id', 'job_id')->withTimestamps();
     }
 }
 
