a25fa561971c093300ae82e8ab80858ab3331d3c
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-04 10:54:31 +0100

    updated

--- app/Models/SavedJob.php
+++ app/Models/SavedJob.php
@@ -9,6 +9,25 @@ class SavedJob extends Model
 {
     use HasFactory;
 
+    
+    protected $fillable = [
+        'role',
+        'user_id',
+        'category_id',
+        'location',
+        'job_type',
+        'category',
+        'description',
+        'salary_range',
+        'yearly_salary',
+        'no_vacancy',
+        'company',
+        'email',
+        'company_description',
+        'website',
+    ];
+
+
 
     public function category(){
         return $this->hasOne(User::class, 'category_id');
