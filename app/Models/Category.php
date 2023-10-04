b4de9b181e812fff2c7412272b64d2a1a9f4c015
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-04 08:02:45 +0100

    updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,11 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'jobList',
+    ];
+
     public function listings(){
         return $this->hasMany(Listing::class, 'category_id');
     }
