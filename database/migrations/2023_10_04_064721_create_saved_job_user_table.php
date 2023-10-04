0845bb44322e2364631c802bc6ed6332f19e98cb
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-04 10:55:12 +0100

    updated

--- database/migrations/2023_10_04_064721_create_saved_job_user_table.php
+++ database/migrations/2023_10_04_064721_create_saved_job_user_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('saved_job_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
             $table->string('role');
+            $table->foreignId('user_id')->constrained();
             $table->string('location');
             $table->string('job_type');
             $table->string('category');
