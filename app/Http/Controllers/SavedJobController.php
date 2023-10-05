dcd249250781e58bdc00bb560b00a9fd7486faec
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-05 21:50:25 +0100

    updated

--- app/Http/Controllers/SavedJobController.php
+++ app/Http/Controllers/SavedJobController.php
@@ -27,7 +27,7 @@ class SavedJobController extends Controller
         ]);
 
         if($jobToSave){
-            return re
+            return 
         }
         // if ($request->hasFile('logo')) {
         //     $formFields['logo'] = $request->file('logo')->store('logos', 'public');
