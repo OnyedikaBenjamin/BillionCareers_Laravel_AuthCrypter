40806cadebd05b6e8cb8e1c0400d038dfa6768e8
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-13 16:02:00 +0100

    updated

--- app/Http/Controllers/SavedJobController.php
+++ app/Http/Controllers/SavedJobController.php
@@ -7,6 +7,7 @@ use App\Models\SavedJob;
 use Illuminate\Http\Request;
 
 class SavedJobController extends Controller
+      SavedJobController
 {
     public function store_saved_jobs(Request $request)
     {
