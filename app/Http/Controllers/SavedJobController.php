cacad5716a3ae40dee6d4b214177e9482a76c9b2
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-13 16:00:33 +0100

    updated

--- app/Http/Controllers/SavedJobController.php
+++ app/Http/Controllers/SavedJobController.php
@@ -10,28 +10,28 @@ class SavedJobController extends Controller
 {
     public function store_saved_jobs(Request $request)
     {
-        // $jobToSave = SavedJob::create([
-        //     'role' => $request->role,
-        //     'job_id' => $request->id,
-        //     'user_id' => $request->user_id,
-        //     'location' => $request->location,
-        //     'job_type' => $request->job_type,
-        //     'description' => $request->description,
-        //     'salary_range' => $request->salary_range,
-        //     'yearly_salary' => $request->yearly_salary,
-        //     'no_vacancy' => $request->no_vacancy,
-        //     'company' => $request->company,
-        //     'email' => $request->email,
-        //     'company_description' => $request->company_description,
-        //     'website' => $request->website,
-        //     'logo' => $request->logo,
-        // ]);
-        // // $foundListing = 
-        // if($jobToSave){
+        $jobToSave = SavedJob::create([
+            'role' => $request->role,
+            'job_id' => $request->id,
+            'user_id' => $request->user_id,
+            'location' => $request->location,
+            'job_type' => $request->job_type,
+            'description' => $request->description,
+            'salary_range' => $request->salary_range,
+            'yearly_salary' => $request->yearly_salary,
+            'no_vacancy' => $request->no_vacancy,
+            'company' => $request->company,
+            'email' => $request->email,
+            'company_description' => $request->company_description,
+            'website' => $request->website,
+            'logo' => $request->logo,
+        ]);
+        // $foundListing = 
+        if($jobToSave){
             return redirect('/listings');
         }
         // if ($request->hasFile('logo')) {
         //     $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         // }
     }
-// }
+}
