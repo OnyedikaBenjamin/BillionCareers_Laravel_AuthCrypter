564fb4dbbbc53ea2231e5f3ad695c3a866712833
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-13 17:16:21 +0100

    updated

--- app/Http/Controllers/SavedJobController.php
+++ app/Http/Controllers/SavedJobController.php
@@ -12,7 +12,7 @@ class SavedJobController extends Controller
     {
         $jobToSave = SavedJob::create([
             'role' => $request->role,
-            'listing_id' => $request->id,
+            'listing_id' => $request->listing_id,
             'user_id' => $request->user_id,
             'location' => $request->location,
             'job_type' => $request->job_type,
