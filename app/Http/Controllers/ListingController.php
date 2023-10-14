7e37a43bb46c56b3d4ae4a563ea2c2024bc81377
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-14 23:43:46 +0100

    updated

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -62,7 +62,7 @@ class ListingController extends Controller
         return view('jobupdated');
     }
 
-    public function storeListing(Request $request)
+    public function (Request $request)
     {
         // dd($request ->file('logo'));
         $formFields = $request->validate([
