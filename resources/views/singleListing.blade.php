51567a85c425e5c8198415fce6ffca05327216fa
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-15 21:42:30 +0100

    updated

--- resources/views/singleListing.blade.php
+++ resources/views/singleListing.blade.php
@@ -214,7 +214,7 @@
                                       <input name="company_description" type="hidden" value="{{$singleListing->company_description}}">
                                       <input name="website" type="hidden" value="{{$singleListing->website}}">
                                       <input name="logo" type="hidden" value="{{$singleListing->logo}}">                   --}}
-                                      <button class="btn btn-block btn-light btn-md"><span class="icon-heart-o mr-2 text-danger">
+                                      <button href= class="btn btn-block btn-light btn-md"><span class="icon-heart-o mr-2 text-danger">
                                       </span>Save Job</button>
                             </div>
                         @endauth
