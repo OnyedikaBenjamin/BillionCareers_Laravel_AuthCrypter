b531d60c0414ba2cf36214e8eb298383c00a55f4
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-15 21:54:17 +0100

    updated

--- resources/views/singleListing.blade.php
+++ resources/views/singleListing.blade.php
@@ -198,6 +198,10 @@
                                       </span>Save Job</button>
                                 </form>
                                 @else
+                                <form method="POST" action="/savedjobs/create" enctype="multipart/form-data">
+                                      <button class="btn btn-block btn-light btn-md" type="submit"><span class="icon-heart-o mr-2 text-danger">
+                                      </span>Save Job</button>
+                                </form>
                                 @endauth
                             </div>
 
