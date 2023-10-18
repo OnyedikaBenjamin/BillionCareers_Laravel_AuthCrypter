0f32ac719507723be7d0fc9b2b6e2f7f7038c860
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-18 01:16:18 +0100

    updated

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])-
 
 Route::post('/savedjobs/create', [SavedJobController::class, 'store_saved_jobs']);
 
-Route::post('/savedjobs/create', [SavedJobController::class, 'store_saved_jobs']);
+Route::get('/savedjobs/create', [SavedJobController::class, 'store_saved_jobs']);
 
 // Route::get('/giging', function(){
 //     return response('<h1>Omo today na giging oo</h1>');
