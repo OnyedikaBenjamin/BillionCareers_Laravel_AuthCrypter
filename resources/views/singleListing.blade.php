63fd020032622c1342b582ed8e1d9f9105c73504
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-18 08:56:55 +0100

    changes effectuated

--- resources/views/singleListing.blade.php
+++ resources/views/singleListing.blade.php
@@ -68,7 +68,7 @@
     <main>
         <div class="slider-area ">
             <div class="single-slider section-overly slider-height2 d-flex align-items-center"
-                data-background="assets/img/hero/about.jpg">
+                 data-background="assets/img/hero/about.jpg">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
