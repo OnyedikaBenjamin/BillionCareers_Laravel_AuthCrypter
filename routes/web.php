9c8f8a86ac6bf32a2bea6644e3883ea94c8adafe
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-19 20:06:37 +0100

    changes effectuated

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/manage', [ListingController::class, 'manageListing']);
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('/categories', [CategoryControl\ler::class, 'store'])->name('categories.store');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
