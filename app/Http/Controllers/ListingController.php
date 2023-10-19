a9f5921486efb516dd394eaaaa78402a39b63cfa
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-19 19:58:32 +0100

    changes effectuated

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -27,7 +27,7 @@ class ListingController extends Controller
     {
         $listings = Listing::select()->take(10)->orderby('id', 'desc')->get();
         $total_listings = Listing::all()->count(); 
-        retun view('listings', compact('listings', 'total_listings'));
+        return view('listings', compact('listings', 'total_listings'));
     }
 
     public function showingleListing($id)
