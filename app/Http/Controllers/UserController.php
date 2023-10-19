e160f0ea884f82a151f1100dc2173b23a3413b1e
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-19 19:59:52 +0100

    changes effectuated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
     }
 
     public function login(){
-        return view('logn');
+        return view('login');
     }
     public function authenticate_user(Request $request){
         $formfields = $request->validate([
