75acad144a1ca384ebf4f3eb2f291116ad0a1c94
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-10-19 20:06:29 +0100

    changes effectuated

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\JobApplicationController;
 use App\Models\Listing;
 use Psy\Command\WhereamiCommand;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SavedJobController;
+use App\Http\Controllers\JobApplicationController;
 
 //LISTINGS ROUTE
 Route::get('/', [ListingController::class, 'go_home']);
@@ -45,7 +46,7 @@ Route::get('/manage', [ListingController::class, 'manageListing']);
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::post('/categories', [CategoryControl\ler::class, 'store'])->name('categories.store');
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
