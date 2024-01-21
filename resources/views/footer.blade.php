16db76b00a2208d41ceb584020ab11fe5e3546a3
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2024-01-21 18:19:27 +0100

    updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -77,8 +77,7 @@
                                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
                                       <!-- logo -->
                                       <div class="footer-logo mb-20">
-                                        {{-- <a href="index.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a> --}}
-                                        <a href="index.html">Billion-Jobs</a>
+                                        <a href="index.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
                                       </div>
                                    </div>
                                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
