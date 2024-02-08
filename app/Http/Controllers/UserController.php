5ee4e6460a278a17d826c81447910ccf6c368472
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2024-02-09 00:28:38 +0100

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,6 @@ class UserController extends Controller
         $formfields['password'] = bcrypt($formfields['password']);
         $user = User::create($formfields);
         auth()->login($user);
-        
         return redirect('/')->with('message', 'Account created successfully');
     }
     public function logout(Request $request){
